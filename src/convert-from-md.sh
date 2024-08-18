a6ff51d55d2771acde5ea73de47aa5fecd535d1d
Robert Landers <landers.robert@gmail.com>
2024-08-18 10:43:52 +0200

    always pull

--- src/convert-from-md.sh
+++ src/convert-from-md.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker run -v "$(pwd)":/data --pull --user "$(id -u)":"$(id -g)" pandoc/latex -t dokuwiki -f gfm "$1" -o "$2"
+docker run -v "$(pwd)":/data --pull always --user "$(id -u)":"$(id -g)" pandoc/latex -t dokuwiki -f gfm "$1" -o "$2"
 # remove all <HTML> and </HTML> tags
 sed -i 's/<HTML>//g' "$2"
 sed -i 's/<\/HTML>//g' "$2"
